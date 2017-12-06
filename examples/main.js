8058d663ac13d727ddd121c1f0d9e068048aa64d
XBT1 <xbtop1@gmail.com>
2017-12-06 23:32:11 +0800

    feat: first blood

---
+++ examples/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import '../src'
+import '../src/styles/index.styl'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+
+import SugarInput from './components/SugarInput.vue'
+
+Vue.component('su-input', SugarInput)
