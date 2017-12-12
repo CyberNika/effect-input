733f561110b0b2d73506ca3bda84cb4551fac8f1
XBT1 <xbtop1@gmail.com>
2017-12-12 23:45:12 +0800

    feat: 添加 Hoshi Kuro Jiro 三套样式

--- examples/App.vue
+++ examples/App.vue
@@ -5,7 +5,9 @@
     </header>
 
     <main>
-      <su-input v-model="value" label="姓名"></su-input>
+      <section v-for="(item, index) of inputList" :key="index">
+        <su-input v-model="item.value" :type="item.type" label="姓名"></su-input>
+      </section>
     </main>
   </div>
 </template>
@@ -16,11 +18,30 @@
 
     data () {
       return {
-        value: '',
+        inputList: [{
+          type: 'haruki',
+          value: '',
+        }, {
+          type: 'hoshi',
+          value: '',
+        }, {
+          type: 'kuro',
+          value: '',
+        }, {
+          type: 'jiro',
+          value: '',
+        }],
       }
     }
   }
 </script>
 
-<style>
+<style scoped>
+  section {
+    text-align: center;
+  }
+
+  section + section {
+    margin-top: 30px;
+  }
 </style>
